5d77bfb1673401df6ce73c96d68d00bc902595d6
Cíntia Fumi <cintiafumi@gmail.com>
2021-03-27 20:12:53 -0300

    ✅  test: list_length

--- test/list_length_test.exs
+++ test/list_length_test.exs
@@ -1,8 +1,15 @@
 defmodule ListLengthTest do
   use ExUnit.Case
-  doctest ListLength
 
-  test "greets the world" do
-    assert ListLength.hello() == :world
+  describe "list_length/1" do
+    test "returns the length of a list" do
+      list = [1, 2, 3]
+
+      response = ListLength.call(list)
+
+      expected_response = 3
+
+      assert response == expected_response
+    end
   end
 end
